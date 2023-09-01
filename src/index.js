92ef980bbd3206a95a2c678a17a3d1738afde3f5
Caio <caiothalles607@gmail.com>
2023-09-01 14:57:23 -0300

    searchExtract

--- src/index.js
+++ src/index.js
@@ -6,21 +6,39 @@ const app = express();
 
 app.use(express.json())
 
-const costumers = []
+const customers = []
 
 app.post("/account", (request, response) => {
     const { cpf, nome } = request.body;
+    
+    const customersAlreadyExist = customers.some(
+        (customers) => customers.cpf === cpf
+    )
 
-    const id = uuidv4();
+    if(customersAlreadyExist) {
+        return response.status(400).json({error: "Customers already exists!"})
+    }
 
-    costumers.push({
+    customers.push({
         cpf,
         nome,
-        id,
+        id: uuidv4(),
         statement: []
     })
 
     return response.status(201).send()
+});
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if (!customer) {
+        return response.json({ error: "Customer not found" });
+    }
+
+    return response.json(customer.statement);
 });
 
 app.listen(3333);
